6415e31934c12cf22e25dd9d46897fce3fd48ecd
codingrev <emmowu10@gmail.com>
2021-04-17 04:40:16 -0700

    upgrade

--- simplysendmail/settings.py
+++ simplysendmail/settings.py
@@ -10,7 +10,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_f7*m%l4w$=3&xuv-isj)%fz6qp1doqs672$2ia3(9mm85bs86'
+SECRET_KEY = os.path.join('SECRET_KEY')
+# SECRET_KEY = '_f7*m%l4w$=3&xuv-isj)%fz6qp1doqs672$2ia3(9mm85bs86'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
